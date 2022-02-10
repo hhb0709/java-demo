8c6cd7734b6faf2028eaed6f6ec0765c037fbd23
hhb0709 <ggvybyun1985@163.com>
2022-02-10 20:53:03 +0800

    java常用代码

---
+++ spring-boot-thymeleaf-tempalte-demo/src/main/java/cn/hhb/springbootthymeleaftempaltedemo/SpringBootThymeleafTempalteDemoApplication.java
@@ -0,0 +1,13 @@
+package cn.hhb.springbootthymeleaftempaltedemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootThymeleafTempalteDemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootThymeleafTempalteDemoApplication.class, args);
+    }
+
+}
---
+++ spring-boot-thymeleaf-tempalte-demo/src/test/java/cn/hhb/springbootthymeleaftempaltedemo/SpringBootThymeleafTempalteDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package cn.hhb.springbootthymeleaftempaltedemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootThymeleafTempalteDemoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
